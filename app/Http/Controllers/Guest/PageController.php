91f0f31275d1e8d5654f21d168edeecc1850fcd1
San-Moro <valentinacortese.dsg@gmail.com>
2022-12-21 21:49:34 +0100

    started with CRUD operations, added index()

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return 'prova';
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
+// Route::get('/', [PageController::class, 'index']);
+
+Route::resource('comics', PageController::class);
